5bdc6f103ed7fe234c2804cb0667753a4b573dae
Dotun Jolaoso <dotunjolaosho@gmail.com>
2022-03-04 17:54:04 +0100

    update delivery attempt endpoints

--- src/Api/DeliveryAttempt.php
+++ src/Api/DeliveryAttempt.php
@@ -4,13 +4,13 @@ namespace Convoy\Api;
 
 class DeliveryAttempt extends AbstractApi
 {
-    public function all(string $eventId): array
+    public function all(string $eventDeliveryId): array
     {
-        return $this->httpGet(sprintf('/events/%s/deliveryattempts', $eventId));
+        return $this->httpGet(sprintf('/eventdeliveries/%s/deliveryattempts',  $eventDeliveryId));
     }
 
-    public function find(string $eventId, string $eventDeliveryId, string $deliveryAttemptId): array
+    public function find(string $eventDeliveryId, string $deliveryAttemptId): array
     {
-        return $this->httpGet(sprintf('/events/%s/eventdeliveries/%s/deliveryattempts/%s', $eventId, $eventDeliveryId, $deliveryAttemptId));
+        return $this->httpGet(sprintf('/eventdeliveries/%s/deliveryattempts/%s', $eventDeliveryId, $deliveryAttemptId));
     }
 }
