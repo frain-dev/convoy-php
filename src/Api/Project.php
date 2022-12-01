a4999c4f46e74f3eac3da02ed6c61536655c4457
Dotun Jolaoso <dotunjolaosho@gmail.com>
2022-12-01 21:53:54 +0100

    update project resource

--- src/Api/Project.php
+++ src/Api/Project.php
@@ -4,17 +4,17 @@ namespace Convoy\Api;
 
 class Project extends AbstractApi
 {
-    public function find(string $id, array $parameters = []): array
+    public function find(array $parameters = []): array
     {
         return $this->httpGet("/", $parameters);
     }
 
-    public function update(string $id, array $data, array $parameters = []): array
+    public function update(array $data, array $parameters = []): array
     {
         return $this->httpPut("/", $data, $parameters);
     }
 
-    public function delete(string $id, array $data = [], array $parameters = []): array
+    public function delete(array $data = [], array $parameters = []): array
     {
         return $this->httpDelete("/", $data, $parameters);
     }
